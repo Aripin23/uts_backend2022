4d26d0f297793379efad5e2042366da807dc03be
Aripin23 <ilhamarifin.official@gmail.com>
2022-11-19 08:11:04 +0700

    add all routes and make rgister login users

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientsController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/patients', [PatientsController::class, 'index']);
+    Route::post('/patients', [PatientsController::class, 'store']);
+    Route::get('/patients/{id}', [PatientsController::class, 'show']);
+    Route::put('/patients/{id}', [PatientsController::class, 'update']);
+    Route::delete('/patients/{id}', [PatientsController::class, 'destroy']);
+    
+    Route::get('/patients/search/{name}', [PatientsController::class, 'search']);
+    Route::get('/patients/status/positive', [PatientsController::class, 'positive']);
+    Route::get('/patients/status/recovered', [PatientsController::class, 'recovered']);
+    Route::get('/patients/status/dead', [PatientsController::class, 'dead']);
+    
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
